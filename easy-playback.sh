b11e7cc1aecf39aec7f0c785074737d7a8e02cda
rany <ranygh@riseup.net>
2021-05-11 16:29:47 +0300

    add stdin support

--- easy-playback.sh
+++ easy-playback.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 ## To use this script you need to install
 ## edge-tts.py to a directory in your $PATH
-## as executable and give it the name edge-tts
+## as executable and give it the name edge-tts.
 trap 'kill -- $(jobs -p) 2>/dev/null' EXIT
-exec {fd}< <(edge-tts "${@}")
+[ "$1" == "read-from-stdin" ] && { stdin=$(cat); shift 1; } || stdin=""
+exec {fd}< <(edge-tts "${@}" <<<"$stdin")
 mpg123 -C "/dev/fd/$fd"
