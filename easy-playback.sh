d7c95d87438e227f76d69a463f43fb9c71027483
rany <ranygh@riseup.net>
2021-05-11 16:35:28 +0300

    improve playback script

--- easy-playback.sh
+++ easy-playback.sh
@@ -3,6 +3,6 @@
 ## edge-tts.py to a directory in your $PATH
 ## as executable and give it the name edge-tts.
 trap 'kill -- $(jobs -p) 2>/dev/null' EXIT
-[ "$1" == "read-from-stdin" ] && { stdin=$(cat); shift 1; } || stdin=""
+[ "$1" == "stdin" ] && { stdin=$(cat); shift 1; set -- "$@" '--file=/dev/stdin'; } || stdin=""
 exec {fd}< <(edge-tts "${@}" <<<"$stdin")
 mpg123 -C "/dev/fd/$fd"
