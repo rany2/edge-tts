6a1d991dd7615e14669e6a74205684280ad969a7
rany <ranygh@riseup.net>
2021-06-08 20:34:18 +0300

    Use subprocess instead of bash

---
+++ src/edgePlayback/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import sys
+import tempfile
+import subprocess
+from shutil import which
+
+def main():
+    if which('mpv') and which('edge-tts'):
+        with tempfile.NamedTemporaryFile() as media:
+            with tempfile.NamedTemporaryFile() as subtitle:
+                print ("Media file: %s" % media.name)
+                print ("Subtitle file: %s" % subtitle.name)
+                p = subprocess.Popen(['edge-tts', '-w'] + sys.argv[1:], stdout=media, stderr=subtitle)
+                p.communicate()
+                p = subprocess.Popen(['mpv', '--keep-open=yes', '--sub-file=' + subtitle.name, media.name])
+                p.communicate()
+    else:
+        print ("This script requires mpv and edge-tts.")
+
+if __name__ == "__main__":
+    main()
