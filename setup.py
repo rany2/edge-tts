a5a9a4c478f86ddb33c08512042bdb8eda7fe041
rany2 <rany2@riseup.net>
2022-09-27 13:05:27 +0300

    Add back GitHub dependabot/dependency graph support
    * Hack from https://github.com/pallets/flask/blob/de16718b39e3544688130339a0813997e3790c48/setup.py#L3-L17

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup()
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="edge-tts",
+    install_requires=[
+        "aiohttp==3.8.3",
+    ],
+)
