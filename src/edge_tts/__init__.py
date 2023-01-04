57c5143aac5b7887e8bc091c549abab490e2d7cb
rany2 <rany2@riseup.net>
2023-01-05 01:13:33 +0200

    complete typing for now

--- src/edge_tts/__init__.py
+++ src/edge_tts/__init__.py
@@ -5,3 +5,5 @@ __init__ for edge_tts
 from .communicate import Communicate
 from .list_voices import VoicesManager, list_voices
 from .submaker import SubMaker
+
+__all__ = ["Communicate", "VoicesManager", "list_voices", "SubMaker"]
