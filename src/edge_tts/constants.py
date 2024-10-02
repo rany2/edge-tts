cb7b893507a0e9f70125c0e462550c3976fac489
Blaise <blaise@applio.org>
2024-10-02 21:05:37 +0200

    use f-strings on constants for better readability

--- src/edge_tts/constants.py
+++ src/edge_tts/constants.py
@@ -3,13 +3,6 @@ Constants for the Edge TTS project.
 """
 
 TRUSTED_CLIENT_TOKEN = "6A5AA1D4EAFF4E9FB37E23D68491D6F4"
-WSS_URL = (
-    "wss://speech.platform.bing.com/consumer/speech/synthesize/"
-    + "readaloud/edge/v1?TrustedClientToken="
-    + TRUSTED_CLIENT_TOKEN
-)
-VOICE_LIST = (
-    "https://speech.platform.bing.com/consumer/speech/synthesize/"
-    + "readaloud/voices/list?trustedclienttoken="
-    + TRUSTED_CLIENT_TOKEN
-)
+
+WSS_URL = f"wss://speech.platform.bing.com/consumer/speech/synthesize/readaloud/edge/v1?TrustedClientToken={TRUSTED_CLIENT_TOKEN}"
+VOICE_LIST = f"https://speech.platform.bing.com/consumer/speech/synthesize/readaloud/voices/list?trustedclienttoken={TRUSTED_CLIENT_TOKEN}"
