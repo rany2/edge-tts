0fb06416515cd19c8f2cf34bc93d39030c0b87e6
帕斯卡 <blaise@applio.org>
2024-10-07 21:51:07 +0200

    Fix CI

--- src/edge_tts/constants.py
+++ src/edge_tts/constants.py
@@ -3,6 +3,7 @@ Constants for the Edge TTS project.
 """
 
 TRUSTED_CLIENT_TOKEN = "6A5AA1D4EAFF4E9FB37E23D68491D6F4"
+BASE_URL = "https://speech.platform.bing.com/consumer/speech/synthesize"
 
-WSS_URL = f"wss://speech.platform.bing.com/consumer/speech/synthesize/readaloud/edge/v1?TrustedClientToken={TRUSTED_CLIENT_TOKEN}"
-VOICE_LIST = f"https://speech.platform.bing.com/consumer/speech/synthesize/readaloud/voices/list?trustedclienttoken={TRUSTED_CLIENT_TOKEN}"
+WSS_URL = f"wss://{BASE_URL}/readaloud/edge/v1?TrustedClientToken={TRUSTED_CLIENT_TOKEN}"
+VOICE_LIST = f"{BASE_URL}/readaloud/voices/list?trustedclienttoken={TRUSTED_CLIENT_TOKEN}"
