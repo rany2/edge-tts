c251d5174f7a7aa3da15a63618a4285f6d5d661f
帕斯卡 <blaise@applio.org>
2024-10-09 10:11:27 +0200

    Update src/edge_tts/constants.py
    Co-authored-by: Rany <rany@riseup.net>

--- src/edge_tts/constants.py
+++ src/edge_tts/constants.py
@@ -6,4 +6,4 @@ TRUSTED_CLIENT_TOKEN = "6A5AA1D4EAFF4E9FB37E23D68491D6F4"
 BASE_URL = "speech.platform.bing.com/consumer/speech/synthesize"
 
 WSS_URL = f"wss://{BASE_URL}/readaloud/edge/v1?TrustedClientToken={TRUSTED_CLIENT_TOKEN}"
-VOICE_LIST = f"{BASE_URL}/readaloud/voices/list?trustedclienttoken={TRUSTED_CLIENT_TOKEN}"
+VOICE_LIST = f"https://{BASE_URL}/readaloud/voices/list?trustedclienttoken={TRUSTED_CLIENT_TOKEN}"
