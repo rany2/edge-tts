8b92ed739a339502b5174c22bc9bf820ea8ba7a1
rany <rany2@riseup.net>
2024-10-25 12:22:16 +0300

    Update Chromium User-Agent version to 130
    Signed-off-by: rany <rany2@riseup.net>

--- src/edge_tts/constants.py
+++ src/edge_tts/constants.py
@@ -8,7 +8,7 @@ TRUSTED_CLIENT_TOKEN = "6A5AA1D4EAFF4E9FB37E23D68491D6F4"
 WSS_URL = f"wss://{BASE_URL}/edge/v1?TrustedClientToken={TRUSTED_CLIENT_TOKEN}"
 VOICE_LIST = f"https://{BASE_URL}/voices/list?trustedclienttoken={TRUSTED_CLIENT_TOKEN}"
 
-CHROMIUM_MAJOR_VERSION = "129"
+CHROMIUM_MAJOR_VERSION = "130"
 BASE_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
     f" (KHTML, like Gecko) Chrome/{CHROMIUM_MAJOR_VERSION}.0.0.0 Safari/537.36"
