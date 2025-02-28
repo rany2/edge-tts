37123878e8f4f27c3761142b23e08406697b6777
Hoàng Đức Mạnh <hoangducmanh03072004@gmail.com>
2025-02-28 22:57:08 +0700

    Update __init__.py

--- src/edge_tts/__init__.py
+++ src/edge_tts/__init__.py
@@ -3,9 +3,14 @@ needing Windows or the Edge browser."""
 
 from . import exceptions
 from .communicate import Communicate
-from .submaker import SubMaker
 from .version import __version__, __version_info__
 from .voices import VoicesManager, list_voices
+import sys
+# Conditionally import the correct SubMaker based on the presence of `--word-by-word`
+if '--word-by-word' in sys.argv:
+    from .submaker_word_by_word import SubMaker  # Import SubMaker for word-by-word processing
+else:
+    from .submaker_sentence import SubMaker  # Import SubMaker for sentence-based processing
 
 __all__ = [
     "Communicate",
